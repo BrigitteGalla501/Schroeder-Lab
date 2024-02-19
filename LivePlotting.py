c33abe4c60cd4f710b8795bbbcfb626a783cc625
BrigitteGalla501 <122285387+BrigitteGalla501@users.noreply.github.com>
2024-02-19 15:02:26 -0500

    Add files via upload
    Csv with Parse: collects data from the Keithley, writes the data into a csv file (one column for voltage, one for time and one for voltage but without units). this should be continuously updating.
    Live plotting: should take the data from the continuously updating csv and make a graph using the voltage without units for the y and time for the x axis.
    LivePlottingfromCSV: this was an attempt to have both the Csv with parse and the live plotting in the same file, but it doesn't work.
    Read csv: is Csv with parse but it only have the time and voltage with units.

---
+++ LivePlotting.py
@@ -0,0 +1,40 @@
+
+from itertools import count
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+
+plt.style.use('fivethirtyeight')
+
+x_vals = []
+y_vals = []
+
+index = count()
+
+
+def animate(i):
+    data = pd.read_csv('New2data.csv')
+    x = data['Run Time']
+    y1 = data['Voltage Parsed']
+   
+
+    plt.cla()
+
+    plt.plot(x, y1, label='Voltage vs Time')
+
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+
+
+ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+
+plt.tight_layout()
+plt.show()
+
+
+    
+  
+
+
